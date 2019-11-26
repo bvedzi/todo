a081c9f02895d952305b1ebbc333dfe3534956d4
Brian Vedzi <vdzvsk@gmail.com>
2019-11-26 19:43:35 +0000

    Lesson 11 completed

--- db/migrate/20191126004318_create_tasks.rb
+++ db/migrate/20191126004318_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.string :done, default: false
+      t.boolean :done, default: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_004318) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "done", default: "f"
+    t.boolean "done", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
